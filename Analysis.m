e27355ac6108111165c922519c01c40246df44e8
Neuron <rishabhbajpai24@gmail.com>
2021-12-10 17:46:32 +0530

    Update Analysis.m

--- Analysis.m
+++ Analysis.m
@@ -5,7 +5,7 @@ Compute_distributions;
 Compare_distributions;
 
 % Sample test AI
-sample_class=1;
+sample_class=1; plots=1;
 sample_index=100;  
 Sample_test_AI;
 Sample_test_report; % rater's scores, 1 is for abnormality and 0 is for normal pattern
